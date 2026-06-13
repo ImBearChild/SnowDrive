a7d91804597dd6c6b5f07f9b37b42b9d0cadf195
Nianqing Yao <imbearchild@outlook.com>
2026-06-13 13:08:54 +0800

    feat: wire iSCSI server into main entry point

--- src/snowscsi_main.c
+++ src/snowscsi_main.c
@@ -1,10 +1,25 @@
+#include <snowscsi/block.h>
+#include <snowscsi/device.h>
+#include <snowscsi/iscsi.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
-  printf("snowscsi: SCSI device emulation toolkit\n");
-  printf("Usage: snowscsi serve [--block <spec>]... [--cdrom <spec>]... "
-         "--iscsi <addr>\n");
-  return 0;
+
+  snowscsi_device_t *dev = snowscsi_block_open_ram(16 * 1024 * 1024);
+  if (!dev) {
+    fprintf(stderr, "snowscsi: failed to create 16MB RAM disk\n");
+    return 1;
+  }
+
+  printf("snowscsi: serving 16MB RAM disk on 0.0.0.0:3260\n");
+
+  snowscsi_device_t *devs[] = {dev};
+  int ret = snowscsi_iscsi_serve(devs, 1, "0.0.0.0:3260", NULL, NULL);
+
+  snowscsi_device_destroy(dev);
+  return ret;
 }
